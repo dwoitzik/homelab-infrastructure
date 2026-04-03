c2401a421a3e17e6419233722adbce6036519635
dwoitzik <david@woitzik.dev>
2026-04-03 18:25:13 +0200

    fix: terraform fmt

--- terraform/stacks/network/vpn.tf
+++ terraform/stacks/network/vpn.tf
@@ -18,17 +18,17 @@ resource "routeros_ip_address" "wg_ip" {
 ###############################################################################
 
 resource "routeros_wireguard_peer" "handy_dw" {
-  interface       = routeros_wireguard.wg_vpn.name
-  comment         = "Smartphone DW - Limited Access"
-  public_key      = "X9iI0RGNf7kTxdBOs4CsDcOQtKRFMYALY/ugHv67uAo="
-  allowed_address = [local.vpn_handy_ip]
+  interface            = routeros_wireguard.wg_vpn.name
+  comment              = "Smartphone DW - Limited Access"
+  public_key           = "X9iI0RGNf7kTxdBOs4CsDcOQtKRFMYALY/ugHv67uAo="
+  allowed_address      = [local.vpn_handy_ip]
   persistent_keepalive = "25s"
 }
 
 resource "routeros_wireguard_peer" "laptop_dw" {
-  interface       = routeros_wireguard.wg_vpn.name
-  comment         = "Laptop DW - Full Admin Access"
-  public_key      = "mE7EAs5FqZ49rGjA71A7p5pPZ6nLzV6/u6u6u6u6u6u="
-  allowed_address = [local.vpn_laptop_ip]
+  interface            = routeros_wireguard.wg_vpn.name
+  comment              = "Laptop DW - Full Admin Access"
+  public_key           = "mE7EAs5FqZ49rGjA71A7p5pPZ6nLzV6/u6u6u6u6u6u="
+  allowed_address      = [local.vpn_laptop_ip]
   persistent_keepalive = "25s"
 }
