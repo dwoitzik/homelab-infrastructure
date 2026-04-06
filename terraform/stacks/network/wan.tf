abfc293409faa2387898865ba898bbe0b39a0e98
dwoitzik <david@woitzik.dev>
2026-04-06 20:55:53 +0200

    add: new fw rule

--- terraform/stacks/network/wan.tf
+++ terraform/stacks/network/wan.tf
@@ -9,21 +9,6 @@ resource "routeros_ip_dhcp_client" "wan_client" {
   use_peer_ntp      = true
 }
 
-resource "routeros_ip_firewall_nat" "masquerade" {
-  chain         = "srcnat"
-  out_interface = "ether1"
-  action        = "masquerade"
-  comment       = "Standard NAT for internet access"
-}
-
-resource "routeros_ip_firewall_filter" "drop_wan_input" {
-  chain            = "input"
-  in_interface     = "ether1"
-  connection_state = "!established,related"
-  action           = "drop"
-  comment          = "Firewall: Drop all external input"
-}
-
 resource "routeros_ip_cloud" "ddns" {
   ddns_enabled = "yes"
   update_time  = true
