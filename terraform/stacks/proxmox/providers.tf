05bdaaa8764af7db6dcad71e09e4722ac89984bf
dwoitzik <david@woitzik.dev>
2026-05-16 21:49:52 +0200

    chore: final homelab optimization and connectivity fixes

--- terraform/stacks/proxmox/providers.tf
+++ terraform/stacks/proxmox/providers.tf
@@ -11,7 +11,7 @@ terraform {
     key    = "proxmox/terraform.tfstate"
     region = "main"
     endpoints = {
-      s3 = "http://10.0.20.252:9000"
+      s3 = "http://minio.apps.svc.cluster.local:9000"
     }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
