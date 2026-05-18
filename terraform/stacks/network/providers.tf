d67518ee0629504b3bcef553a8ba921aa1591349
dwoitzik <david@woitzik.dev>
2026-05-18 11:35:46 +0200

    fix(network): fix S3 backend for Minio compatibility

--- terraform/stacks/network/providers.tf
+++ terraform/stacks/network/providers.tf
@@ -9,14 +9,18 @@ terraform {
   }
 
   backend "s3" {
-    bucket                      = "terraform-state"
-    key                         = "network/terraform.tfstate"
-    region                      = "main"
-    endpoint                    = "http://minio.apps.svc.cluster.local:9000"
+    bucket = "terraform-state"
+    key    = "network/terraform.tfstate"
+    region = "main"
+    endpoints = {
+      s3 = "http://minio.apps.svc.cluster.local:9000"
+    }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
   }
 }
 
