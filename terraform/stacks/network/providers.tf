9eaa6040b4f0d83f7839f3eb3585802952ba80a6
dwoitzik <david@woitzik.dev>
2026-05-21 19:56:48 +0200

    fix(terraform): use bucket ID instead of alias for state

--- terraform/stacks/network/providers.tf
+++ terraform/stacks/network/providers.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-state"
+    bucket = "8de1a93a0d33539b"
     key    = "network/terraform.tfstate"
     region = "homelab"
     endpoints = {
