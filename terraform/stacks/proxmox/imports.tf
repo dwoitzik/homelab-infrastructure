e57f7211326b8b32dafa4f522f2193be08436ff6
dwoitzik <david@woitzik.dev>
2026-06-13 18:55:55 +0200

    feat(terraform): add Proxmox import blocks for existing infrastructure
    All 8 resources (5 LXCs + 3 k3s VMs) have lifecycle ignore_changes guards
    so Atlantis plan will import state without proposing destructive changes.
    Garage S3 backend is now operational (layout configured, bucket created).

---
+++ terraform/stacks/proxmox/imports.tf
@@ -0,0 +1,39 @@
+import {
+  to = proxmox_virtual_environment_container.ct_mgmt_pbs_01
+  id = "pve-mgmt-01/110"
+}
+
+import {
+  to = proxmox_virtual_environment_container.ct_srv_docker_01
+  id = "pve-mgmt-01/200"
+}
+
+import {
+  to = proxmox_virtual_environment_container.ct_srv_ai_01
+  id = "pve-mgmt-01/201"
+}
+
+import {
+  to = proxmox_virtual_environment_container.ct_dmz_proxy_01
+  id = "pve-mgmt-01/301"
+}
+
+import {
+  to = proxmox_virtual_environment_container.ct_dmz_games_01
+  id = "pve-mgmt-01/302"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_srv_k3s_11_master
+  id = "pve-mgmt-01/211"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_srv_k3s_12_worker
+  id = "pve-mgmt-01/212"
+}
+
+import {
+  to = proxmox_virtual_environment_vm.vm_srv_k3s_13_worker
+  id = "pve-mgmt-01/213"
+}
